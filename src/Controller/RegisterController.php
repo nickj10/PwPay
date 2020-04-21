bdbab6b97fc96e6ed13ba08eb7437c36499839d4
Kaye Ann Ignacio <keianignacio@gmail.com>
2020-04-21 22:08:36 +0200

    Update RegisterController.php

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -8,7 +8,7 @@ use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
-final class LoginController
+final class RegisterController
 {
     private ContainerInterface $container;
 
