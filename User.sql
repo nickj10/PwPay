b38f8013cc48ab171362a06097eaccfb5aa8d120
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-04-27 21:24:08 +0200

    Update User table creation script

--- User.sql
+++ User.sql
@@ -1,11 +1,12 @@
 CREATE DATABASE IF NOT EXISTS pwpay; 
 USE pwpay; 
 CREATE TABLE IF NOT EXISTS user(
-	user_id BIGINT AUTO_INCREMENT,
-	email VARCHAR(255),
-	password VARCHAR(255),
-	birthday date,
-	phone INTEGER,
-	status VARCHAR(255) DEFAULT 'inactive',
-  	PRIMARY KEY (user_id)
+    user_id BIGINT AUTO_INCREMENT,
+    email VARCHAR(255),
+    password VARCHAR(255),
+    birthday date,
+    phone INTEGER,
+    balance FLOAT DEFAULT 0.0,
+    status VARCHAR(255) DEFAULT 'inactive',
+    PRIMARY KEY (user_id)
 );
