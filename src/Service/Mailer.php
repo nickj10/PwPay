e6629d19d2042010547186322a6db7f80aabddaf
Kaye Ann Ignacio <keianignacio@gmail.com>
2020-05-02 13:34:47 +0200

    Update Mailer.php

--- src/Service/Mailer.php
+++ src/Service/Mailer.php
@@ -22,8 +22,7 @@ class Mailer {
             $mail->Port = 587;
             $mail->isHTML(true);
             $mail->setFrom('kayeann.ignacio@students.salle.url.edu', 'Pwpay');
-            //$mail->addAddress($to);
-            $mail->addAddress('kayeann.sn@gmail.com');
+            $mail->addAddress($to);
             $mail->Subject = 'Activation Link';
             $mail->Body = 'Welcome to Pwpay! Click the following link to activate your account: http://' . $_SERVER['HTTP_HOST'] . '/activate?token=' . $token;
             $mail->send();
