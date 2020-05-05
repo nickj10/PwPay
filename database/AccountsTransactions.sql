266befd6f3f77f9f37532cf7fefa780390f3317f
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-05-06 01:31:04 +0200

    feat(WEBII-54): Make PKs autoincrement

--- database/AccountsTransactions.sql
+++ database/AccountsTransactions.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS Accounts (
-    account_id bigint NOT NULL, 
+    account_id BIGINT AUTO_INCREMENT, 
     user_id bigint,
     owner_name VARCHAR(255),
     iban VARCHAR(50),
     balance FLOAT DEFAULT 0.0,
-    activity_status BOOLEAN DEFAULT false,
+    activity_status BOOLEAN DEFAULT true,
     FOREIGN KEY (user_id) REFERENCES user(user_id),
     PRIMARY KEY (account_id)
 );
 
 CREATE TABLE IF NOT EXISTS Transactions (
-    transactions_id bigint NOT NULL, 
+    transactions_id BIGINT AUTO_INCREMENT, 
     user_id bigint,
     account_id bigint,
     amount FLOAT DEFAULT 0.0,
