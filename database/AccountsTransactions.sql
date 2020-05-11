2bf17100b1a0bdaeda820cc34edec8a810741894
Nicole Jimenez <nicole.jimenez@adevinta.com>
2020-05-11 19:09:28 +0200

    feat(WEBII-66): Add created_at and modified_at

--- database/AccountsTransactions.sql
+++ database/AccountsTransactions.sql
@@ -5,6 +5,8 @@ CREATE TABLE IF NOT EXISTS Accounts (
     iban VARCHAR(50),
     balance FLOAT DEFAULT 0.0,
     activity_status BOOLEAN DEFAULT true,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+	modified_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES user(user_id),
     PRIMARY KEY (account_id)
 );
@@ -15,6 +17,7 @@ CREATE TABLE IF NOT EXISTS Transactions (
     account_id bigint,
     amount FLOAT DEFAULT 0.0,
     action VARCHAR(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES user(user_id),
     FOREIGN KEY (account_id) REFERENCES Accounts(account_id),
     PRIMARY KEY (transactions_id)
